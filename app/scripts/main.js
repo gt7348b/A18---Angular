afe0a6a55990a10a91f0f72351db789c5e6898d9
John Crocker <johnniecrocker@yahoo.com>
2014-11-18 13:56:09 -0500

    basic setup of app and route config

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,20 @@
-console.log('The Iron Yard Rocks');
+(function(){
+
+  angular.module('TranspoData', ['ngRoute'])
+
+  .constant ({
+
+    'Url': 'http://tiy-atl-fe-server.herokuapp.com/collections/transpodata/'
+
+  })
+
+  .config(function($routeProvider){
+
+    console.log('here');
+
+  })
+
+
+
+
+}());
