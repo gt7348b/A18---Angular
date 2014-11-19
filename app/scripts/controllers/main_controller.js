9726609b022afad388dcc5026566e69a3cf77fc4
John Crocker <johnniecrocker@yahoo.com>
2014-11-19 13:56:00 -0500

    retreiving data, not displaying

--- app/scripts/controllers/main_controller.js
+++ app/scripts/controllers/main_controller.js
@@ -1,9 +1,15 @@
 (function(){
 
   angular.module('TranspoData')
-  .controller('MainController', ['$scope', '$http', 'Url', function($scope, $http, Url){
+  .controller('MainController', ['$scope', '$http', '$location','Url', function($scope, $http, $location, Url){
 
     console.log('main');
+
+    $http.get(Url).success( function(results){
+
+      $scope.list = results;
+      console.log(results);
+    });
 
   }]);
 
